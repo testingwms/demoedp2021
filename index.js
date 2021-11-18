e7e204514125207fe7cc7c0bf0bb76648b0e98fd
testingwms <93272071+testingwms@users.noreply.github.com>
2021-11-18 14:05:45 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function myMove(){
+    var elem = document.getElementById("animate");
+    var pos = 0;
+    var id = setInterval(frame, 5);
+
+    function frame()
+    {
+        if(pos===350){
+            clearInterval(id);
+        }else{
+           pos++;
+           elem.style.left = pos + 'px';
+           elem.style.bottom = pos + 'px';
+        }
+    }
+}
+
+
